244f9782ae518b6441c05ca753fff231b81b9ab4
HaiTran <hai.tran@kyanon.digital>
2022-10-08 16:50:24 +0700

    Sat 10/08/2022 16:50:24.42

--- service/healthcheck.service.go
+++ service/healthcheck.service.go
@@ -3,21 +3,19 @@ package service
 import (
 	"eCommerce/dto"
 	"eCommerce/util"
-	"fmt"
 	gosysinfo "github.com/elastic/go-sysinfo"
 	"time"
 )
 
 func HealthCheck() dto.HealthCheckRes {
-	res := dto.HealthCheckRes{}
-	res.Code = util.SUCCESS_CODE
-	res.Message = "Service is ok"
-
 	process, _ := gosysinfo.Self()
 	processInfo, _ := process.Info()
 	startAt := processInfo.StartTime
 	uptime := time.Now().Sub(startAt).String()
-	fmt.Println(process.CPUTime())
+
+	res := dto.HealthCheckRes{}
+	res.Code = util.SUCCESS_CODE
+	res.Message = "Service is ok"
 	res.Data.Uptime = uptime
 
 	return res
