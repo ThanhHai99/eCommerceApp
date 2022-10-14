203dcce6c22526f92dd9c679376d85006ec6a205
HaiTran <hai.tran@kyanon.digital>
2022-10-14 08:07:57 +0700

    Fri 10/14/2022  8:07:57.08

---
+++ model/sale.model.go
@@ -0,0 +1,26 @@
+package model
+
+type Sale struct {
+	BaseModel
+	Name string `gorm:"not null;" json:"name"`
+}
+
+func (Sale) TableName() string {
+	return "sale"
+}
+
+type SaleUpdateRequest struct {
+	//Id          *string          `json:"id"`
+	//Name        *string          `json:"name" `
+	//Key         *string          `json:"key"`
+	//Value       *string          `json:"value"`
+	//JsonValue   *postgres.Jsonb  `json:"json_value"`
+	//Type        *string          `json:"type"`
+	//Url         *pq.StringArray  `json:"url"`
+	//IsActive    *bool            `json:"is_active"`
+	//PlatformKey *string          `json:"platform_key"`
+	//UpdaterID   *uuid.UUID       `json:"updater_id"`
+	//UrlEn       *pq.StringArray  `json:"url_en"`
+	//OrderBy     *int             `json:"order_by"`
+	//Advanced    *json.RawMessage `json:"advanced"`
+}
---
+++ model/sale_log.model.go
@@ -0,0 +1,26 @@
+package model
+
+type SaleLog struct {
+	BaseModel
+	Name string `gorm:"not null;" json:"name"`
+}
+
+func (SaleLog) TableName() string {
+	return "sale_log"
+}
+
+type SaleLogUpdateRequest struct {
+	//Id          *string          `json:"id"`
+	//Name        *string          `json:"name" `
+	//Key         *string          `json:"key"`
+	//Value       *string          `json:"value"`
+	//JsonValue   *postgres.Jsonb  `json:"json_value"`
+	//Type        *string          `json:"type"`
+	//Url         *pq.StringArray  `json:"url"`
+	//IsActive    *bool            `json:"is_active"`
+	//PlatformKey *string          `json:"platform_key"`
+	//UpdaterID   *uuid.UUID       `json:"updater_id"`
+	//UrlEn       *pq.StringArray  `json:"url_en"`
+	//OrderBy     *int             `json:"order_by"`
+	//Advanced    *json.RawMessage `json:"advanced"`
+}
