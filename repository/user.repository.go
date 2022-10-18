565bd979aa9fa327ffb81d53e065de6c48949907
HaiTran <hai.tran@kyanon.digital>
2022-10-19 00:56:40 +0700

    Wed 10/19/2022  0:56:40.16

--- repository/user.repository.go
+++ repository/user.repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"eCommerce/model"
+	"github.com/google/uuid"
 )
 
 func CreateNew(user *model.User) error {
@@ -11,14 +12,18 @@ func CreateNew(user *model.User) error {
 	return err
 }
 
+func GenerateToken(username string) error {
+	db, _ := model.Connect()
+	defer db.Close()
+	err := db.Where(model.User{Username: username}).Update(&model.User{}, model.User{VerifyToken: uuid.New().String()}).Error
+	return err
+}
+
 func CheckExists(username string) bool {
 	db, _ := model.Connect()
 	defer db.Close()
-	exist := db.Model(&model.User{}).First(&model.User{}, model.User{Username: username})
-	if exist.RowsAffected == 0 {
-		return false
-	}
-	return true
+	notFound := db.Model(&model.User{}).First(&model.User{}, model.User{Username: username}).RecordNotFound()
+	return !notFound
 }
 
 func IsHaveVerifyToken(token string) *model.User {
@@ -26,13 +31,20 @@ func IsHaveVerifyToken(token string) *model.User {
 	defer db.Close()
 	record := model.User{}
 	//record = db.Model(&model.User{}).First(&model.User{}, model.User{VerifyToken: token})
-	db.First(&record, model.User{VerifyToken: token})
+	_ = db.First(&record, model.User{VerifyToken: token}).Error
 	return &record
 }
 
 func ActiveAccount(username string) error {
 	db, _ := model.Connect()
 	defer db.Close()
-	 err := db.Where(model.User{Username: username}).Update(model.User{IsActive: true}).Error
-	 return err
+	err := db.Where(model.User{Username: username}).Update(model.User{IsActive: true}).Error
+	return err
+}
+
+func GetOneByUsername(username string) (user *model.User) {
+	db, _ := model.Connect()
+	defer db.Close()
+	_ = db.First(&user, model.User{Username: username}).Error
+	return user
 }
--- service/authenticate.service.go
+++ service/authenticate.service.go
@@ -6,6 +6,8 @@ import (
 	"eCommerce/repository"
 	"eCommerce/util"
 	"encoding/json"
+	"golang.org/x/crypto/bcrypt"
+	"reflect"
 )
 
 func Register(registerBody *dto.RegisterBody) dto.RegisterRes {
@@ -23,6 +25,8 @@ func Register(registerBody *dto.RegisterBody) dto.RegisterRes {
 			res.Code = util.FAIL_CODE
 			res.Message = "Server error"
 		} else {
+			_ = repository.GenerateToken(registerBody.Username)
+			// Send mail
 			res.Code = util.SUCCESS_CODE
 			res.Message = "Register successfully"
 		}
@@ -33,7 +37,23 @@ func Register(registerBody *dto.RegisterBody) dto.RegisterRes {
 
 func Login(loginBody dto.LoginBody) dto.LoginRes {
 	res := dto.LoginRes{}
+	user := repository.GetOneByUsername(loginBody.Username)
+	if user != nil {
+		res.Code = util.FAIL_CODE
+		res.Message = "The username is not exists"
+	} else {
+		err1 := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginBody.Password))
+		if err1 == nil {
+			if !reflect.DeepEqual(user, model.User{}) {
 
+			}
+			res.Code = util.SUCCESS_CODE
+			res.Message = "Login successfully"
+		} else {
+			res.Code = util.FAIL_CODE
+			res.Message = "The credential is invalid"
+		}
+	}
 	return res
 }
 
